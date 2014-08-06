4eb6f9f42b09812cf002fa12ce9fca84d49af5af
neethugk <neethu.gk@bridge-india.in>
2014-08-06 12:35:24 +0530

    latest commit QuestionAnswerpost

---
+++ SlidingCiby2/src/com/bridge/bridgepmt/interfaces/PMTQuestionAnswerPOstListner.java
@@ -0,0 +1,11 @@
+package com.bridge.bridgepmt.interfaces;
+
+import com.bridge.bridgepmt.model.PMTEvaluationQuestionsScreenReturns;
+
+public interface PMTQuestionAnswerPOstListner {
+
+	void postSuccess(PMTEvaluationQuestionsScreenReturns pMTEvaluationQuestionsScreenReturns);
+
+	
+
+}
